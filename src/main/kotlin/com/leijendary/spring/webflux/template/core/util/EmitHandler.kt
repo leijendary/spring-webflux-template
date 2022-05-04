da15905867ae4bc994a5865de55281373c2720b4
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-05-04 12:22:11 +0800

    removed log in EmitHandler

--- src/main/kotlin/com/leijendary/spring/webflux/template/core/util/EmitHandler.kt
+++ src/main/kotlin/com/leijendary/spring/webflux/template/core/util/EmitHandler.kt
@@ -1,17 +1,8 @@
 package com.leijendary.spring.webflux.template.core.util
 
-import com.leijendary.spring.webflux.template.core.extension.logger
-import reactor.core.publisher.SignalType
 import reactor.core.publisher.Sinks.EmitFailureHandler
-import reactor.core.publisher.Sinks.EmitResult
 import reactor.core.publisher.Sinks.EmitResult.FAIL_NON_SERIALIZED
 
 object EmitHandler {
-    private val log = logger()
-
-    val emitFailureHandler = EmitFailureHandler { signalType: SignalType, emitResult: EmitResult ->
-        log.warn("Sink emission failure signal type {} and result {}", signalType, emitResult)
-
-        emitResult == FAIL_NON_SERIALIZED
-    }
+    val emitFailureHandler = EmitFailureHandler { _, emitResult -> emitResult == FAIL_NON_SERIALIZED }
 }
